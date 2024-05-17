c8730fb464578cc528f82e2b1daddf4b6bfba8d7
Gustav Dahmén <gd.dahmen@gmail.com>
2024-05-17 17:11:25 +0200

    add api to tmdb. recommendation search work with info

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for, flash
+from tmdbv3api import TMDb, Movie
+import pandas as pd
+from assets import API_KEY, token
+import requests
+
+app = Flask(__name__)
+app.secret_key = token
+tmdb = TMDb()
+tmdb.api_key = API_KEY
+movie = Movie()
+
+rated_movies = pd.DataFrame(columns=["title", "release_date", "popularity", "genre_ids", "vote_average", "rating"])
+
+def fetch_genre_list():
+    url = "https://api.themoviedb.org/3/genre/movie/list?language=en"
+    headers = {
+        "accept": "application/json",        
+        'Authorization': f'Bearer {token}'
+    }
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        genre_list = response.json()['genres']
+        genre_dict = {genre['id']: genre['name'] for genre in genre_list}
+        
+        
+        return genre_dict
+    else:
+        return None
+
+@app.route('/')
+def index():
+    genre_dict = fetch_genre_list()
+    genre_list = list(genre_dict.values())
+    if request.method == 'POST':
+
+        recommendations = get_recommendations()
+        return render_template('index.html', recommendations=recommendations, genres= genre_list)
+    
+    else:
+  
+        genre_dict = fetch_genre_list()
+        genre_list = list(genre_dict.values())
+        return render_template('index.html',  genres= genre_list)
+
+@app.route('/recommendations', methods=['POST'])
+def get_recommendations():
+    movie_name = request.form['movie_name']
+    search_result = movie.search(movie_name)
+    if search_result:
+        first_movie_id = search_result[0].id
+        recommendations = movie.recommendations(first_movie_id)
+
+        recommendations_list = list(recommendations)
+
+        recommendations_list.sort(key=lambda x: (-x.popularity, -x.vote_average, x.release_date))
+
+        recommendations = recommendations_list[:15]
+        
+        genre_dict = fetch_genre_list()
+
+        for recommendation in recommendations:
+            recommendation.genre_names = [genre_dict.get(genre_id, 'Unknown') for genre_id in recommendation.genre_ids]       
+                
+        return render_template('recommendations.html', recommendations=recommendations)
+    else:
+        flash('No recommendations found for the entered movie.', 'error')
+        return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
