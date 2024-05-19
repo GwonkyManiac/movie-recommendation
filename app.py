002167a25212e8ae8535e062c65ac7971c073061
Gustav Dahmén <gd.dahmen@gmail.com>
2024-05-19 19:23:28 +0200

    possible to filter of genres

--- app.py
+++ app.py
@@ -23,31 +23,29 @@ def fetch_genre_list():
     if response.status_code == 200:
         genre_list = response.json()['genres']
         genre_dict = {genre['id']: genre['name'] for genre in genre_list}
-        
-        
         return genre_dict
     else:
         return None
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     genre_dict = fetch_genre_list()
     genre_list = list(genre_dict.values())
     if request.method == 'POST':
-
         recommendations = get_recommendations()
-        return render_template('index.html', recommendations=recommendations, genres= genre_list)
-    
+        return render_template('index.html', recommendations=recommendations, genres=genre_list)
     else:
-  
-        genre_dict = fetch_genre_list()
-        genre_list = list(genre_dict.values())
-        return render_template('index.html',  genres= genre_list)
+        return render_template('index.html', genres=genre_list)
 
 @app.route('/recommendations', methods=['POST'])
 def get_recommendations():
+    selected_genres = request.form.getlist('genres[]')
     movie_name = request.form['movie_name']
     search_result = movie.search(movie_name)
+    
+    # Print the selected genres for debugging
+    print("Selected genres:", selected_genres)
+    
     if search_result:
         first_movie_id = search_result[0].id
         recommendations = movie.recommendations(first_movie_id)
@@ -59,15 +57,23 @@ def get_recommendations():
         recommendations = recommendations_list[:15]
         
         genre_dict = fetch_genre_list()
-
-        for recommendation in recommendations:
-            recommendation.genre_names = [genre_dict.get(genre_id, 'Unknown') for genre_id in recommendation.genre_ids]       
+        
+        # Filter movies based on selected genres
+        if selected_genres:
+            filtered_movies = [
+                rec for rec in recommendations
+                if any(genre_dict.get(genre_id) in selected_genres for genre_id in rec.genre_ids)
+            ]
+        else:
+            filtered_movies = recommendations
+        
+        for recommendation in filtered_movies:
+            recommendation.genre_names = [genre_dict.get(genre_id, 'Unknown') for genre_id in recommendation.genre_ids]
                 
-        return render_template('recommendations.html', recommendations=recommendations)
+        return render_template('recommendations.html', recommendations=filtered_movies)
     else:
         flash('No recommendations found for the entered movie.', 'error')
         return redirect(url_for('index'))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
